0aaed60f27cc8a919cc52a6a186e60846c5834aa
Kraks <kiss.kraks@gmail.com>
2014-05-22 10:32:44 +0800

    add cond/else

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Inspired by Peter Norvig's [Lisp.py](http://norvig.com/lispy.html), I take two days and wrote a tiny lisp interpreter in JavaScript.
 
-Meanwhile, I add support of `let`, `let*`, `and`, `or` and some other syntax sugar. 
+Meanwhile, I added support of `let`, `let*`, `cond`, `else`, `and`, `or` and some other syntax sugar. Now it could interpret a Y combinator!
 
-Dependencies: underscore.js
+Dependencies: [underscore.js](http://underscorejs.org)
 
 Have fun!
 
--- lisp.js
+++ lisp.js
@@ -6,6 +6,7 @@ var Env = function(params, args, outer) {
     var outer = outer;
     _.extend(dict, _.object(params, args));
     this.find = function(name) {
+        //console.log(name);
         if (_.has(dict, name)) return that;
         return outer.find(name);
     };
@@ -25,6 +26,7 @@ var add_globals = function(env) {
         "-":       f_template(function(x, y) { return x-y; }),
         "*":       f_template(function(x, y) { return x*y; }),
         "/":       f_template(function(x, y) { return x/y; }),
+        "else":    true,
         "not":     function(val)  { return !val; },
         "and":     function(x, y) { return x && y; },
         "or":      function(x, y) { return x || y; },
@@ -74,6 +76,12 @@ var eval = function(x, env) {
         });
         return eval(["let", t, x[2]], env);
     }
+    else if (x[0] === "cond") {
+        return eval(_.find(x.slice(1), function(item) { return eval(item[0], env); })[1], env);
+    }
+    else if (x[0] === "else") {
+        return eval(x[1], env);
+    }
     else if (x[0] === "set!") {
         env.find(x[1]).set(x[1], eval(x[2], env));
     }
@@ -86,7 +94,7 @@ var eval = function(x, env) {
         };
     }
     else if (x[0] === "begin") {
-        var end = x.prototype.slice(-1);
+        var end = x.pop();
         _.each(x, function(ele) {
             eval(ele, env);
         });
@@ -117,7 +125,7 @@ var read = function(s) { return read_from(tokenize(s)); };
 var parse = read;
 
 var tokenize = function(s) {
-    return _.filter(s.replace(/\(/g, " ( ").replace(/\)/g, " ) ").split(" "),
+    return _.filter(s.replace(/[\r\t\n]/g, " ").replace(/\(/g, " ( ").replace(/\)/g, " ) ").split(" "),
              function(item) { return !_.isEmpty(item); });
 };
 
@@ -158,47 +166,53 @@ var to_string = function(exp) {
     return exp.toString();
 };
 
-console.log(eval(parse("(+ 1 2 3)")))
-console.log(eval(parse("(* 2 3 4)")))
+console.log(eval(parse("(+ 1 2 3)"))) //6
+console.log(eval(parse("(* 2 3 4)"))) //24
 
-console.log(eval(parse("(car (list 1 2 3))")));
-console.log(eval(parse("(cdr (list 1 2 3))")));
-console.log(eval(parse("(cons 1 2)")));
-console.log(eval(parse("(cons 1 (list 2 3))")));
-console.log(eval(parse("(length (list 9 9 9))")));
-console.log(eval(parse("(< 1 2)")));
-console.log(eval(parse("(<= 1 2)")));
-console.log(eval(parse("(= 2 2)")));
-console.log(eval(parse("(list? (list 1 2 3))")));
-console.log(eval(parse("(null? (list))"))); 
+console.log(eval(parse("(car (list 1 2 3))"))); //1
+console.log(eval(parse("(cdr (list 1 2 3))"))); //[2, 3]
+console.log(eval(parse("(cons 1 2)"))); //[1, 2]
+console.log(eval(parse("(cons 1 (list 2 3))"))); //[1, 2, 3]
+console.log(eval(parse("(length (list 9 9 9))"))); //3
+console.log(eval(parse("(< 1 2)"))); //true
+console.log(eval(parse("(<= 1 2)"))); // true
+console.log(eval(parse("(= 2 2)"))); //true
+console.log(eval(parse("(list? (list 1 2 3))"))); //true
+console.log(eval(parse("(list? (quote abc))"))); //false
+console.log(eval(parse("(null? (list))"))); //true
 
-eval(parse("(define add (lambda (x y) (+ x y)))"));
-console.log(eval(parse("(add 2 4)")));
+eval(parse("(define add (lambda (x y) (+ x y)))")); 
+console.log(eval(parse("(add 2 4)"))); //6
 
 eval(parse("(define area (lambda (r) (* 3.141592653 (* r r))))"));
-console.log(eval(parse("(area 3)")));
+console.log(eval(parse("(area 3)"))); //28.2xxxxx
 
 eval(parse("(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))"));
-console.log(eval(parse("(fact 10)")));
-console.log(eval(parse("(if (equal? 1 1) (quote conseq) (quote otherwise))")));
-console.log(eval(parse("(if (equal? 1 2) (quote conseq) (quote otherwise))")));
-console.log(eval(parse("(not (equal? 1 1))")));
+console.log(eval(parse("(fact 10)"))); //3628800
+console.log(eval(parse("(if (equal? 1 1) (quote conseq) (quote otherwise))"))); //conseq
+console.log(eval(parse("(if (equal? 1 2) (quote conseq) (quote otherwise))"))); //otherwise
+console.log(eval(parse("(not (equal? 1 1))"))); // false
 
 eval(parse("(define first car)"));
 eval(parse("(define rest cdr)"));
 
-console.log(eval(parse("(first (list 1 2 3))")));
-console.log(eval(parse("(rest (list 1 2 3))")));
-console.log(eval(parse("(equal? 1 (first (list 1 2 3)))")));
+console.log(eval(parse("(first (list 1 2 3))"))); //1
+console.log(eval(parse("(rest (list 1 2 3))"))); //[2, 3]
+console.log(eval(parse("(equal? 1 (first (list 1 2 3)))"))); //true
 
 eval(parse("(define count (lambda (item L) (if (not (null? L)) (+ (equal? item (first L)) (count item (rest L))) 0)))"));
 console.log(eval(parse("(count 2 (list 0 1 2 2 0 2))"))); //3
 
 console.log(eval(parse("(count 0 (list 0 1 2 3 0 2))"))); //2
 
-console.log(eval(parse("(and (equal? 1 2) (equal? 2 2))")));
-console.log(eval(parse("(or (equal? 1 2) (equal? 2 2))")));
+console.log(eval(parse("(and (equal? 1 2) (equal? 2 2))"))); //false
+console.log(eval(parse("(or (equal? 1 2) (equal? 2 2))"))); //true
 
-console.log(eval(parse("(let ((x 5)) x)")));
+console.log(eval(parse("(cond ((> (area 3) 30) (quote should_not_happen)) ((equal? 1 2) (quote should_not_happen)) (else (quote else_happen)))")));
+console.log(eval(parse("(let ((x 5)) x)"))); //5
 console.log(eval(parse("(let ((x 5) (y 2)) (+ x y))"))); //7
 console.log(eval(parse("(let* ((x 5) (y (+ x 2))) (+ x y))"))); //12
+
+eval(parse("(define Y (lambda (le) ((lambda (f) (f f)) (lambda (f) (le (lambda (x) ((f f) x)))))))"));
+eval(parse("(define FactY (Y (lambda (f) (lambda (n) (cond ((eq? n 0) 1) (else (* n (f (- n 1)))))))))"));
+console.log(eval(parse("(FactY 10)"))); //3628800
