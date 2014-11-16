255c9cf4079f7711b7868e69b6d6b1ecc1d25876
Kraks <kiss.kraks@gmail.com>
2014-11-16 23:30:34 +0800

    add test case

--- test/test.scm
+++ test/test.scm
@@ -8,8 +8,37 @@
 
 (length (list 5 5 5))
 
+(< 1 2)
+
+(<= 1 2)
+
+(= 2 2)
+
+(list? (list 1 2 3))
+
+(list? (quote abc))
+
+(null? (list))
+
+(define fact
+  (lambda (n)
+    (if (<= n 1) 1
+        (* n (fact (- n 1))))))
+
+(fact 10)
+
+(define count
+  (lambda (item L)
+    (if (not (null? L))
+        (+ (equal? item (car L)) (count item (cdr L)))
+        0)))
+
+(count 2 (list 0 1 2 2 1 3))
+
 (define add
   (lambda (x y) (+ x y)))
+
+(add 1 2)
 
 (let ((x 5) (y 2)) (+ x y))
 
@@ -25,3 +54,14 @@
                (else (* n (f (- n 1)))))))))
 
 (FactY 10)
+
+(define U
+  (lambda (F) (F F)))
+
+(define FactU
+  (U (lambda (f)
+       (lambda (n)
+         (if (<= n 0) 1
+             (* n ((U f) (- n 1))))))))
+
+(FactU 10)
